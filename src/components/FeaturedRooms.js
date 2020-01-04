1429d235909312e7652e93b04942f5d974e7d376
Muema <jmuema95@gmail.com>
2020-01-04 21:51:50 +0300

    finished up on the featured romms component

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react';
+import {RoomContext} from '../context';
+import Loading from '../components/Loading';
+import Room from './Room';
+import Title from './Title';
+
+class FeaturedRooms extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    static contextType = RoomContext
+
+    render() {
+        let { loading, featuredRooms : rooms } = this.context
+        rooms = rooms.map( room => {
+            return <Room key={room.id} room={room}/>
+        })
+        return (
+            <section className="featured-rooms">
+                <Title title="featured rooms"/>
+                <div className="featured-rooms-center">{loading?<Loading/>:rooms}</div>
+            </section>
+        )
+    }
+}
+
+export default FeaturedRooms
